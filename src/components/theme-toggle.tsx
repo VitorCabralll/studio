aef66fe31e5606e05bebba24fa36f0d0b0d73e3f
Vitor Hugo Cabral Araujo <VitorCabral703@gmail.com>
2025-06-26 08:09:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,12 +1,31 @@
+
 "use client"
 
+import * as React from "react"
 import { useTheme } from "next-themes"
+
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Sun, Moon, Laptop } from "lucide-react"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+      <div className="grid grid-cols-3 gap-4">
+        <Skeleton className="h-[98px] w-full" />
+        <Skeleton className="h-[98px] w-full" />
+        <Skeleton className="h-[98px] w-full" />
+      </div>
+    )
+  }
 
   return (
     <RadioGroup
